952f5dff59f51cdb30b8153b6f9e1ab77abbd760
Rishabhkr11 <rishabhkr1107@gmail.com>
2021-04-07 22:34:58 +0530

    Initial commit

---
+++ users/includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="site-footer">
+          <div class="text-center">
+              2021 - PBL Group 13
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
